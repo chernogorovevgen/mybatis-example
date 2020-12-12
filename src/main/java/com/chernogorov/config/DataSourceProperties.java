d3b0a2113566d722e382074b22cfb3046b6b50d4
Evgeniy <chernogorovevgen@gmail.com>
2020-12-12 13:45:03 +0700

    Добавлена зависимость lombok

--- src/main/java/com/chernogorov/config/DataSourceProperties.java
+++ src/main/java/com/chernogorov/config/DataSourceProperties.java
@@ -1,5 +1,11 @@
 package com.chernogorov.config;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DataSourceProperties {
 
     private String driverClassName;
@@ -8,107 +14,11 @@ public class DataSourceProperties {
     private String password;
     private String connectionTestQuery;
     private String poolName;
-    private Long connectionTimeout;
-    private Long idleTimeout;
-    private Long validationTimeout;
+    private long connectionTimeout;
+    private long idleTimeout;
+    private long validationTimeout;
     private int maximumPoolSize;
     private int minimumIdle;
     private boolean autoCommit;
 
-
-    public String getDriverClassName() {
-        return driverClassName;
-    }
-
-    public void setDriverClassName(String driverClassName) {
-        this.driverClassName = driverClassName;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConnectionTestQuery() {
-        return connectionTestQuery;
-    }
-
-    public void setConnectionTestQuery(String connectionTestQuery) {
-        this.connectionTestQuery = connectionTestQuery;
-    }
-
-    public String getPoolName() {
-        return poolName;
-    }
-
-    public void setPoolName(String poolName) {
-        this.poolName = poolName;
-    }
-
-    public Long getConnectionTimeout() {
-        return connectionTimeout;
-    }
-
-    public void setConnectionTimeout(Long connectionTimeout) {
-        this.connectionTimeout = connectionTimeout;
-    }
-
-    public Long getIdleTimeout() {
-        return idleTimeout;
-    }
-
-    public void setIdleTimeout(Long idleTimeout) {
-        this.idleTimeout = idleTimeout;
-    }
-
-    public Long getValidationTimeout() {
-        return validationTimeout;
-    }
-
-    public void setValidationTimeout(Long validationTimeout) {
-        this.validationTimeout = validationTimeout;
-    }
-
-    public int getMaximumPoolSize() {
-        return maximumPoolSize;
-    }
-
-    public void setMaximumPoolSize(int maximumPoolSize) {
-        this.maximumPoolSize = maximumPoolSize;
-    }
-
-    public int getMinimumIdle() {
-        return minimumIdle;
-    }
-
-    public void setMinimumIdle(int minimumIdle) {
-        this.minimumIdle = minimumIdle;
-    }
-
-    public boolean isAutoCommit() {
-        return autoCommit;
-    }
-
-    public void setAutoCommit(boolean autoCommit) {
-        this.autoCommit = autoCommit;
-    }
 }
